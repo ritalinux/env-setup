2d62f07a9966ad86b4f068cd4ed0df3e04f06ad3
ritalinux <wedrano1999@gmail.com>
2024-09-23 22:29:03 -0300

    feat(plugings): add telescope

--- nvim/lua/core/packer.lua
+++ nvim/lua/core/packer.lua
@@ -30,6 +30,7 @@ return require('packer').startup(function(use)
 	use('wbthomason/packer.nvim')
 	use('nvim-lualine/lualine.nvim')
 	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-telescope/telescope.nvim', { tag = '0.1.5' })
 
     use {
         "windwp/nvim-autopairs",
